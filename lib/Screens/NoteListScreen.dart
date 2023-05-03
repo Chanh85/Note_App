c3299d9683dd9c8417164e9ea5038a57c2253829
Chanh85 <chanh773er@yahoo.com>
2023-05-03 18:29:29 +0700

    Add labeling feature(to be updated)

--- lib/Screens/NoteListScreen.dart
+++ lib/Screens/NoteListScreen.dart
@@ -17,19 +17,22 @@ class _NoteListScreenState extends State<NoteListScreen> {
       'id': 'kl3091j2j2',
       'title': 'Việt Nam khởi đầu tốt nhất lịch sử dự U20 châu Á',
       'content':
-          'Lần đầu trong lịch sử 64 năm của giải U20 châu Á, Việt Nam thắng cả hai trận đầu tiên, trước Australia và Qatar.'
+          'Lần đầu trong lịch sử 64 năm của giải U20 châu Á, Việt Nam thắng cả hai trận đầu tiên, trước Australia và Qatar.',
+      'lable': ''
     },
     {
       'id': '21343242434',
       'title': 'Truyền thông Indonesia khen U20 Việt Nam phi thường',
       'content':
-          'Nhiều báo, đài Indonesia ngạc nhiên khi Việt Nam toàn thắng hai trận đầu ở bảng đấu khó tại vòng chung kết U20 châu Á 2023'
+          'Nhiều báo, đài Indonesia ngạc nhiên khi Việt Nam toàn thắng hai trận đầu ở bảng đấu khó tại vòng chung kết U20 châu Á 2023',
+      'lable': ''
     },
     {
       'id': 'sk20918340',
       'title': 'Hai nghi can cướp ngân hàng ở Sài Gòn bị bắt',
       'content':
-          'Ngày 5/3, Trương Minh Thiện và đồng phạm Trương Vĩnh Xương, 34 tuổi, bị Công an TP HCM phối hợp Cục cảnh sát Hình sự (C02, Bộ Công an) bắt về hành vi Cướp tài sản. Trong đó, Thiện bị xác định vai trò cầm đầu. Khẩu súng người này sử dụng khi gây án là loại bắn đạn bi nhựa.'
+          'Ngày 5/3, Trương Minh Thiện và đồng phạm Trương Vĩnh Xương, 34 tuổi, bị Công an TP HCM phối hợp Cục cảnh sát Hình sự (C02, Bộ Công an) bắt về hành vi Cướp tài sản. Trong đó, Thiện bị xác định vai trò cầm đầu. Khẩu súng người này sử dụng khi gây án là loại bắn đạn bi nhựa.',
+      'lable': ''
     },
     {
       'id': 'l091283-k29',
@@ -41,36 +44,44 @@ Cụ thể, cử phạt vi phạm hành chính không lập biên bản được
 
 Điều 56 cũng nêu ngoại lệ: Nếu vi phạm hành chính được phát hiện nhờ sử dụng phương tiện, thiết bị kỹ thuật, nghiệp vụ thì phải lập biên bản.
 
-Theo điều luật trên, luật sư Hải cho hay dù mức phạt dưới 250.000 đồng (cá nhân) mà vi phạm được phát hiện thông qua phương tiện, kỹ thuật, nghiệp vụ thì vẫn phải lập biên bản. Với trường hợp phát hiện vi phạm tại chỗ mà mức phạt dưới 250.000 đồng thì không cần lập biên bản.'''
+Theo điều luật trên, luật sư Hải cho hay dù mức phạt dưới 250.000 đồng (cá nhân) mà vi phạm được phát hiện thông qua phương tiện, kỹ thuật, nghiệp vụ thì vẫn phải lập biên bản. Với trường hợp phát hiện vi phạm tại chỗ mà mức phạt dưới 250.000 đồng thì không cần lập biên bản.''',
+      'lable': ''
     },
     {
       'id': 'lkku0918230',
       'title': 'Cắt giảm nhân sự lan rộng trong ngành địa ốc',
       'content':
-          'Công ty anh Hoàng làm việc có trụ sở tại quận 3, TP HCM, đã cắt giảm nhân sự từ cuối năm 2022 nhưng tình hình ngày càng trầm trọng hơn trong hơn 2 tháng đầu năm 2023. Từ tháng 1 đến tháng 2, bên cạnh số nhân viên bị công ty cho thôi việc còn có nhiều người chủ động xin nghỉ vì thu nhập bị cắt giảm mạnh không đủ trang trải cuộc sống. Hoàng cho hay hiện các phòng ban của công ty đều giảm nhân sự 50-70% do hoạt động đầu tư và bán hàng đều đình trệ.'
+          'Công ty anh Hoàng làm việc có trụ sở tại quận 3, TP HCM, đã cắt giảm nhân sự từ cuối năm 2022 nhưng tình hình ngày càng trầm trọng hơn trong hơn 2 tháng đầu năm 2023. Từ tháng 1 đến tháng 2, bên cạnh số nhân viên bị công ty cho thôi việc còn có nhiều người chủ động xin nghỉ vì thu nhập bị cắt giảm mạnh không đủ trang trải cuộc sống. Hoàng cho hay hiện các phòng ban của công ty đều giảm nhân sự 50-70% do hoạt động đầu tư và bán hàng đều đình trệ.',
+      'lable': ''
     },
     {
       'id': '122098390245l',
       'title': 'Ngành vận tải biển qua thời hoàng kim',
       'content':
-          'Khi nhìn về khả năng phục hồi đơn hàng của ngành gỗ, ông Trần Lam Sơn, Tổng giám đốc Thiên Minh - nhà xuất khẩu sang châu Âu - nhận thấy chi phí vận chuyển là một trong những cơ hội.'
+          'Khi nhìn về khả năng phục hồi đơn hàng của ngành gỗ, ông Trần Lam Sơn, Tổng giám đốc Thiên Minh - nhà xuất khẩu sang châu Âu - nhận thấy chi phí vận chuyển là một trong những cơ hội.',
+      'lable': ''
     },
     {
       'id': 'lkj9882901',
       'title':
           'Doanh nghiệp phát hành trái phiếu được kéo dài kỳ hạn thêm 2 năm',
       'content':
-          'Cụ thể, trước đây doanh nghiệp không được thay đổi kỳ hạn trái phiếu đã phát hành, nhưng quy định mới sửa đổi cho phép kéo dài thời hạn của trái phiếu thêm tối đa 2 năm. Trong trường hợp trái chủ không đồng ý thay đổi này, doanh nghiệp "phải có trách nhiệm đàm phán để đảm bảo quyền lợi của nhà đầu tư". Nếu quá trình đàm phán vẫn không đạt kết quả như mong đợi, doanh nghiệp phải thực hiện nghĩa vụ với trái chủ theo phương án phát hành trái phiếu đã công bố.'
+          'Cụ thể, trước đây doanh nghiệp không được thay đổi kỳ hạn trái phiếu đã phát hành, nhưng quy định mới sửa đổi cho phép kéo dài thời hạn của trái phiếu thêm tối đa 2 năm. Trong trường hợp trái chủ không đồng ý thay đổi này, doanh nghiệp "phải có trách nhiệm đàm phán để đảm bảo quyền lợi của nhà đầu tư". Nếu quá trình đàm phán vẫn không đạt kết quả như mong đợi, doanh nghiệp phải thực hiện nghĩa vụ với trái chủ theo phương án phát hành trái phiếu đã công bố.',
+      'lable': ''
     },
   ];
 
   bool listView = true;
-
   var _passcontroller1 = TextEditingController();
   var _passcontroller2 = TextEditingController();
   bool validate = false;
-
   Offset _tapPos = Offset.zero;
+  String selectedValue = 'Personal';
+  var labelItems = ['Work', 'Personal', 'Family'];
+
+
+
+
   void _getTapPos(TapDownDetails tapPos){
     final RenderBox renderBox = context.findRenderObject() as RenderBox;
     setState(() {
@@ -79,9 +90,6 @@ Theo điều luật trên, luật sư Hải cho hay dù mức phạt dưới 250
     });
   }
 
-  void _showOptionNoteListScreen(context) async {
-
-  }
 
   void _showContextMenu(context, note) async{
     final RenderObject? overlay = Overlay.of(context).context.findRenderObject();
@@ -97,15 +105,57 @@ Theo điều luật trên, luật sư Hải cho hay dù mức phạt dưới 250
                 leading: Icon(Icons.delete),
                 title: Text('Delete'),
               ),
+          ),PopupMenuItem(
+            value: 'label',
+            child: ListTile(
+              leading: Icon(Icons.label_outline),
+              title: Text('Label'),
+            ),
           ),
+
         ]);
     switch(res){
       case 'delete' : _onDelete(notes.indexOf(note), Actions.delete);
       break;
+      case 'label': _label(note);
+      break;
     }
   }
 
-
+  void _label(note) async
+  {
+    showDialog(
+        context: context,
+        builder: (ctx) => StatefulBuilder(
+          builder: (ctx, setDialogState) => AlertDialog(
+            title: Text('Choose label'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: labelItems.map((e) => RadioListTile(
+                title: Text(e),
+                  value: e,
+                  groupValue: selectedValue,
+                  onChanged: (v){
+                    setState(() {
+                      setDialogState((){
+                        selectedValue = v!;
+                      });
+                    });
+                    // Navigator.pop(context);
+                  })).toList(),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: (){
+                    note['lable'] = selectedValue;
+                    print(note['lable']);
+                    Navigator.pop(context);},
+                  child: Text('OK')
+              ),
+            ],
+          ),
+        ));
+  }
 
   bool _handleSubmit()
   {
@@ -301,6 +351,11 @@ Theo điều luật trên, luật sư Hải cho hay dù mức phạt dưới 250
               label: 'Protect',
               onPressed: (context) => _onProtect(note, lock, Actions.protect)
           ),
+          SlidableAction(
+            backgroundColor: Colors.lightGreenAccent,
+              icon: Icons.label_outline,
+              label: 'Label',
+              onPressed: (context) => _label(note))
         ],
       ),
       endActionPane: ActionPane(
@@ -352,37 +407,6 @@ Theo điều luật trên, luật sư Hải cho hay dù mức phạt dưới 250
         appBar: AppBar(
           title: Text('Note Management'),
           actions: [
-            // IconButton(
-            //     onPressed: (){
-            //       showDialog(
-            //           context: context,
-            //           barrierDismissible: false,
-            //           builder: (ct) => AlertDialog(
-            //             title: Text('Logout?'),
-            //             content: Text('Are you sure want to logout?'),
-            //             actions: [
-            //               TextButton(
-            //                   onPressed: (){
-            //                     Navigator.popUntil(context, ModalRoute.withName('/'));
-            //                   },
-            //                   child: Text('Yes')
-            //               ),
-            //               TextButton(
-            //                   onPressed: (){Navigator.pop(context);},
-            //                   child: Text('No')
-            //               ),
-            //             ],
-            //           ));
-            //     },
-            //     icon: Icon(Icons.logout)),
-            // IconButton(
-            //   onPressed: () {
-            //     setState(() {
-            //       listView = !listView;
-            //     });
-            //   },
-            //   icon: Icon(listView ? Icons.grid_view : Icons.list),
-            // ),
             PopupMenuButton(
               onSelected: (value) {
                 if(value == 'changeview'){
@@ -412,6 +436,23 @@ Theo điều luật trên, luật sư Hải cho hay dù mức phạt dưới 250
                         ],
                       ));
                   }
+                else if(value == 'View_label')
+                  {
+                    //TODO: view label
+                    showModalBottomSheet(
+                        context: context,
+                        builder: (ctx) =>ListView(
+                          shrinkWrap: true,
+                          children: labelItems.map((e) => ListTile(
+                            title: Text(e),
+                            onTap: (){
+                              Navigator.pop(context);
+                              print(e);
+                            },
+                          )).toList(),
+                        )
+                    );
+                  }
               },
                 itemBuilder: (context) => [
               PopupMenuItem(
@@ -422,7 +463,11 @@ Theo điều luật trên, luật sư Hải cho hay dù mức phạt dưới 250
                 value: 'logout',
                   child: ListTile(leading: Icon(Icons.logout),
               title: Text('Logout'),)),
-            ])
+              PopupMenuItem(
+                  value: 'View_label',
+                  child: ListTile(leading: Icon(Icons.label_important_outlined),
+                    title: Text('View labeled items'),)),
+                ],)
           ],
           automaticallyImplyLeading: false,
         ),
